36c0cbada7af64919124bd0910aaf8ff806b7cad
antonioneto-hotmart <91587245+antonioneto-hotmart@users.noreply.github.com>
2022-08-17 17:43:40 -0300

    rename models

--- models/streams/users.sql
+++ models/streams/users.sql
@@ -1,8 +1,8 @@
 with
     source_data as (
         select
-        accountid as id_account,
         displayname as display_name,
+        accountid as id_account,
         emailaddress as email,
         _airbyte_emitted_at as airbyte_emitted_at
 
